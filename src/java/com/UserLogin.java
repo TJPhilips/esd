81cb1eb03f9cdbcba860c43243b91084f07611d4
thedoctor2016 <mapowell96@gmail.com>
2017-11-16 17:36:42 +0000

    Commit

---
+++ src/java/com/UserLogin.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.JOptionPane;
+import model.Jdbc;
+
+/**
+ *
+*/
+public class UserLogin extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        HttpSession session = request.getSession();
+
+        Jdbc jdbc = new Jdbc();
+        jdbc.connect((Connection) request.getServletContext().getAttribute("connection"));
+        session.setAttribute("dbbean", jdbc);
+
+        if (jdbc == null) {
+            request.getRequestDispatcher("/WEB-INF/conErr.jsp");
+        } else {
+
+            String id = request.getParameter("id");
+            String password = request.getParameter("password");
+
+            if (jdbc.checkUser(id, password)) {
+                if (id.equals("admin") && password.equals("admin")) {
+                    request.getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
+                    session.setAttribute("userID", "admin");
+                
+            } else {
+                    request.getRequestDispatcher("/WEB-INF/userPage.jsp").forward(request, response);
+                    session.setAttribute("userID", id);
+            }
+
+            } else {
+                request.getRequestDispatcher("/WEB-INF/userLoginFail.jsp").forward(request, response);
+            }
+        }
+
+    }
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
