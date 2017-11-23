9f2e1382d6ac01b5bab0fddf03ffec642de62528
a2-gloyne <33129085+a2-gloyne@users.noreply.github.com>
2017-11-23 16:55:27 +0000

    Update UserServlet.java
    Removed unnecessary comments.

--- ESD/web/WEB-INF/JAVA/UserServlet.java
+++ ESD/web/WEB-INF/JAVA/UserServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com;
 
 import java.io.IOException;
@@ -69,8 +64,6 @@ public class UserServlet extends HttpServlet {
         }
     }
       
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
